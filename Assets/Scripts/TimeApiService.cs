f7082a448ff0803f48a840d073ccd89c869f9463
Jafarov Jafar <jafarovja@yandex.ru>
2023-03-26 23:00:33 +0700

    второй сервис

--- Assets/Scripts/TimeApiService.cs
+++ Assets/Scripts/TimeApiService.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
 using Cysharp.Threading.Tasks;
 
 public class TimeApiService : TimeService
 {
-    public override UniTask<TimeSpan> GetTime()
+    private string _url = "https://www.timeapi.io/api/Time/current/coordinate?latitude=48.8534&longitude=2.3488";
+
+    public override async UniTask<TimeSpan> GetTime()
     {
-        //UnityWebRequest.Get()
-        return new UniTask<TimeSpan>();
+        string some = (await UnityWebRequest.Get(_url).SendWebRequest()).downloadHandler.text;
+        Root result = JsonUtility.FromJson<Root>(some);
+
+        int hours = Convert.ToInt32(result.hour);
+        int minutes = Convert.ToInt32(result.minute);
+        int seconds = Convert.ToInt32(result.seconds);
+
+        TimeSpan resultTime = new TimeSpan(hours, minutes, seconds);
+
+        return resultTime;
+    }
+
+    private class Root
+    {
+        public int year;
+        public int month;
+        public int day;
+        public int hour;
+        public int minute;
+        public int seconds;
+        public string date;
     }
 }
